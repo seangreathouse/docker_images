3b2e4a68eece515d5685610e4169e83b6a3792cb
Sean Greathouse <sean@sgreathouse.net>
2023-10-05 10:31:40 -0700

    fixed smee url

--- jenkins/smee.sh
+++ jenkins/smee.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # install in /usrt/local/bin
-smee --url  https://smee.techdelivery-cust.cloud/1Vd7z4RSgptln0Gj --path /github-webhook/ --port 8080 >> /var/log/smee.log 2>&1 &
+smee --url $1 --path /github-webhook/ --port 8080 >> /var/log/smee.log 2>&1 &
