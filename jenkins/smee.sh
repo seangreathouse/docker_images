b7865e437db84e1ab8f1c64395d5cac8902152cf
Sean Greathouse <sean@sgreathouse.net>
2023-10-23 09:57:36 -0700

    fixed smee

--- jenkins/smee.sh
+++ jenkins/smee.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # install in /usrt/local/bin
-smee --url $1 --path /github-webhook/ --port 8080 >> /var/log/smee.log 2>&1 &
+# export SMEE_URL="https://smee.io/1234567890"
+# export SMEE_TOKEN="foo"
+# smee.sh $SMEE_URL $SMEE_TOKEN
+# docker run --env SMEE_URL=$SMEE_URL SMEE_TOKEN=$SMEE_TOKEN -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home ghcr.io/seangreathouse/docker_images/jenkins
+# Generic Webhook Path
+smee --url $1 --path /generic-webhook-trigger/invoke?token=$2 --port 8080 >> /var/log/smee.log 2>&1 &
